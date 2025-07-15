e9a1083600369e2bcab2ec2974667dd8ed8b5bc2
吳昱萱 <meteorshower542@gmail.com>
2025-07-15 04:00:40 -0700

    Add test code

---
+++ test_sample.py
@@ -0,0 +1,10 @@
+# test_sample.py
+
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    assert 3 - 1 == 2
+
+def test_fail_case():
+    assert 2 * 2 == 5  # 這一行會故意讓 CI 失敗（測試紅燈情境）
